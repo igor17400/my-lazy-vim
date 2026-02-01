35596a965605efd4fd8c468247b1e51d608f1e10
Igor Lima Rocha <igorlima1740@gmail.com>
2026-02-01 08:34:51 -0300

    feat: beter highlight color for lazygit

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,17 +4,25 @@ return {
     "folke/snacks.nvim",
     opts = function(_, opts)
       -- Set dim highlight to be visible on light background
-      -- Adjust the color to your preference:
-      --   #928374 = lighter (more transparent look)
-      --   #7c6f64 = medium
-      --   #504945 = darker (more visible)
       vim.api.nvim_set_hl(0, "SnacksDim", { fg = "#7c6f64" })
+      -- Custom highlights for lazygit
+      vim.api.nvim_set_hl(0, "LazyGitSelectedLine", { bg = "#d5c4a1" }) -- brown for active panel
+      vim.api.nvim_set_hl(0, "LazyGitInactiveSelected", { bg = "#d5c4a1" }) -- dark gray for inactive panel
 
       vim.api.nvim_create_autocmd("ColorScheme", {
         callback = function()
           vim.api.nvim_set_hl(0, "SnacksDim", { fg = "#7c6f64" })
+          vim.api.nvim_set_hl(0, "LazyGitSelectedLine", { bg = "#d5c4a1" })
+          vim.api.nvim_set_hl(0, "LazyGitInactiveSelected", { bg = "#d5c4a1" })
         end,
       })
+
+      -- Custom lazygit theme
+      opts.lazygit = opts.lazygit or {}
+      opts.lazygit.theme = {
+        selectedLineBgColor = { bg = "LazyGitSelectedLine" }, -- active panel
+        inactiveViewSelectedLineBgColor = { bg = "LazyGitInactiveSelected" }, -- inactive panel (diff view)
+      }
 
       return opts
     end,
